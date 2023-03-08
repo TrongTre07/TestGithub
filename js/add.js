50b24af97de00333621b2b9c118c83857e129ab9
TrongTre07 <trongtruth2003@gmail.com>
2023-03-08 23:05:49 +0700

    branch example

---
+++ js/add.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const add = () => {
+  return (
+    <View>
+      <Text>Anh trong dep trai</Text>
+    </View>
+  )
+}
+
+export default add
+
+const styles = StyleSheet.create({})
